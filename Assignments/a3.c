251d86ddff4160537ca62e4ab55af6d2dff1ef2f
Anshuman Sharma <rsanshuman01@gmail.com>
2026-09-20 10:38:36 +0530

    second commit

---
+++ Assignments/a3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+// int main()
+// {
+//     char x;
+//     printf("Enter a character (only one)");
+//     scanf("%c",&x);
+//     printf("%d",x);
+//     printf("\n");
+//     return 0;
+// }
+
+// int main()
+// {
+//     int x;
+//     printf("Enter a number(in btw 0 to 255)");
+//     scanf("%d",&x);
+//     printf("It's ASCII code is %c",x);
+//     printf("\n");
+//     return 0;
+// }
+
+// int main()
+// {
+//     char a,b,c;
+//     printf("Enter three characters ");
+//     scanf("%c %c %c",&a,&b,&c);
+//     printf("%c -- %d ,%c -- %d ,%c -- %d",a,a,b,b,c,c);
+//     printf("\n");
+//     return 0;
+// }
+
--- C Language/first.c
+++ C Language/first.c
@@ -39,15 +39,45 @@
 //     return 0;
 // }
 
+// int main()
+// {
+//     int x=5,y;
+//     y=++x;
+//     printf("%d %d", x,y);
+
+//     printf("\n");
+//     return 0;
+
+
+
+// }
+
 int main()
 {
-    int x=5,y;
-    y=++x;
-    printf("%d %d", x,y);
+    int a;
+    double x;
+    float y;
+    char z; 
+     // By printing the sizeof char variable we will get 1 but if we direct prints the char.. constant like 'a' the we will get 4 byte as answer because we know that all charc.. have their ascii keys means numbers which are stored later in the form of 0s and 1s but when we declare a variable of char type then it will print 1 beacuse one char will always be of 1 byte size.....  
+    
+    
+     // x=sizeof(float); -- 4
+    // y=sizeof(double); -- 8
+    // z=sizeof(int); --4
 
+
+    // x = sizeof(34);  -- 4
+    // y=sizeof('A');   -- 4
+    // z=sizeof(2.34);  -- 8 (because any real constant is of double type by default )
+
+    a = sizeof(x);
+    printf("\n%d",a);
+    a=sizeof(y);
+    printf("\n%d",a);
+    a =sizeof(z);
+    printf("\n%d",a); 
+
+    // printf("%d\n%f\n%d",x,y,z);
     printf("\n");
     return 0;
-
-
-
 }
