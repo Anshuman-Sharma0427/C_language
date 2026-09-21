5e78c018f64d564bff8f45350a93d1f30b35d89c
Anshuman Sharma <rsanshuman01@gmail.com>
2026-09-21 20:55:31 +0530

    updated

--- Assignments/a4.c
+++ Assignments/a4.c
@@ -40,11 +40,12 @@
 //     return 0;
 // }
 
-int main()
-{
-    int x=5,y=6;
-    x=y;
-    printf("%d %d",x,y);
-    printf("\n");
-    return 0;
-}
+// int main()
+// {
+//     int x=5,y=6;
+//     x = x+(y-x);
+//     y = x-1;
+//     printf("%d %d",x,y);
+//     printf("\n");
+//     return 0;
+// }
