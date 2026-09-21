fdf29aec3eaa8daa36ddfd4d6b7ff24e074d80be
Anshuman Sharma <rsanshuman01@gmail.com>
2026-09-21 14:48:16 +0530

    fourth comit

---
+++ Assignments/a4.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+// doubt------
+// int main()
+// {
+//     int x;
+//     x = sizeof('A');
+//     printf("%d",x);
+//     printf("\n");
+//     return 0;
+// }
+
+// int main()
+// {
+//     int x;
+//     x=sizeof(34.2345);
+//     printf("%d",x);
+//     printf("\n");
+//     return 0 ;
+// }
+
+// int main()
+// {
+//     char ch='A';
+//     // printf("%d",ch);
+//     ch++;
+//     // printf("\n%d",ch);
+//     printf("\n%c",ch);
+//     printf("\n");
+//     return 0;
+// }
+
+// int main()
+// {
+//     int x=6,y=5,z=x;
+//     x=y;
+//     y=z;
+//     printf("%d %d",x,y);
+//     printf("\n");
+//     return 0;
+// }
+
+int main()
+{
+    int x=5,y=6;
+    x=y;
+    printf("%d %d",x,y);
+    printf("\n");
+    return 0;
+}
